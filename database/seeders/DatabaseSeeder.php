5f7436c02d543245890169b25a003e259a25af20
musawenkos <ndlelamusa1st@gmail.com>
2022-10-28 20:20:07 +0200

    genre list

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\GenreList;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +21,90 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        /* GenreList::create([
+            'id' => 28,
+            'name' => 'Action'
+        ]);
+        GenreList::create([
+            'id' => 12,
+            'name' => 'Adventure'
+        ]);
+
+        GenreList::create([
+            'id' => 16,
+            'name' => 'Animtion'
+        ]);
+        GenreList::create([
+            'id' => 35,
+            'name' => 'Comedy'
+        ]);
+
+        GenreList::create([
+            'id' => 80,
+            'name' => 'Crime'
+        ]);
+        GenreList::create([
+            'id' => 99,
+            'name' => 'Documentary'
+        ]);
+
+        GenreList::create([
+            'id' => 18,
+            'name' => 'Drama'
+        ]);
+        GenreList::create([
+            'id' => 10751,
+            'name' => 'Family'
+        ]);
+
+        GenreList::create([
+            'id' => 14,
+            'name' => 'Fantasy'
+        ]);
+        GenreList::create([
+            'id' => 36,
+            'name' => 'History'
+        ]);
+
+        GenreList::create([
+            'id' => 27,
+            'name' => 'Horror'
+        ]);
+        GenreList::create([
+            'id' => 10402,
+            'name' => 'Music'
+        ]);
+
+        GenreList::create([
+            'id' => 9648,
+            'name' => 'Mystery'
+        ]);
+        GenreList::create([
+            'id' => 10749,
+            'name' => 'Romance'
+        ]);
+
+        GenreList::create([
+            'id' => 878,
+            'name' => 'Science Fiction'
+        ]);
+        GenreList::create([
+            'id' => 10770,
+            'name' => 'TV Movie'
+        ]);
+
+        GenreList::create([
+            'id' => 53,
+            'name' => 'Thriller'
+        ]);
+        GenreList::create([
+            'id' => 10752,
+            'name' => 'War'
+        ]);
+
+        GenreList::create([
+            'id' => 37,
+            'name' => 'Western'
+        ]); */
     }
 }
