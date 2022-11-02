f61669986f2c12cf356c0b479b5012cbe0d6ee18
musawenkos <ndlelamusa1st@gmail.com>
2022-11-02 11:10:20 +0200

    change movie-info design

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,6 +17,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
         <title>Heiman Pictures Weaving</title>
         <link rel="stylesheet" href="{{asset('css/main.css')}}"/>
     </head>
