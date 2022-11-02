3ba705d53f92e50f9f93709f7eff283d4bc3c6a6
musawenkos <ndlelamusa1st@gmail.com>
2022-11-02 13:53:02 +0200

    removed

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -58,3 +58,8 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
+{{-- <form class="d-flex me-2">
+    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-success" type="submit">Search</button>
+  </form>
+ --}}
